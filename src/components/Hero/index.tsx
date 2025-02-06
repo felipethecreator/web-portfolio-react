f852c4b7695e7977ea941f5afc5ba217bb7e0493
felipethecreator <feliperodriguesqueiroz122@gmail.com>
2025-02-06 20:34:31 -0300

    feat: added down arrow and fixed it on the page

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,19 +1,23 @@
 import './style.css'
+import downArrow from '../../assets/down-arrow.png'
 
 function Hero() {
     return (
-        <div className='heroContainer'>
-            <div className='heroPersonalInfos'>
-                <h1 className='heroName'>Felipe Rodrigues</h1>
-                <h2 className='heroCargo'>Dev Full-Stack</h2>
+        <>
+            <div className='heroContainer'>
+                <div className='heroPersonalInfos'>
+                    <h1 className='heroName'>Felipe Rodrigues</h1>
+                    <h2 className='heroCargo'>Dev Full-Stack</h2>
+                </div>
+                <hr />
+                <p className='heroText'>
+                    Desafio a lógica para criar soluções que não apenas funcionam, mas também encantam.
+                    Como desenvolvedor Full Stack, minha missão é construir pontes entre ideias e realidade,
+                    um pixel de cada vez. 🚀💻✨
+                </p>
             </div>
-            <hr/>
-            <p className='heroText'>
-                Desafio a lógica para criar soluções que não apenas funcionam, mas também encantam.
-                Como desenvolvedor Full Stack, minha missão é construir pontes entre ideias e realidade,
-                um pixel de cada vez. 🚀💻✨
-            </p>
-        </div>
+            <img className='heroArrow' src={downArrow} />
+        </>
     )
 }
 
